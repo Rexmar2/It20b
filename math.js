9272712b12ef5192376fae4205ad8a40b7aeff53
Rexmar2 <hazerulincuna@gmail.com>
2025-11-18 08:09:18 +0800

    Added funtion for basic arithmetic functions

---
+++ math.js
@@ -0,0 +1,48 @@
+function sum(num1,num2){
+    sum = num1 + num2
+    console.log(sum);
+}
+sum(2,3);
+
+
+function diff(num1,num2){
+    sum = num1 - num2
+    console.log(diff);
+}
+diff(2,3);
+
+function prod(num1,num2){
+    sum = num1 * num2
+    console.log(prod);
+}
+prod(2,3);
+
+function quo(num1,num2){
+    sum = num1 / num2
+    console.log(quo);
+}
+quo(2,3);
+
+// function add(num1,num2){
+//     return num1+num2;
+
+// }
+
+// function diff(num1,num2){
+//     return num1-num2;
+// }
+
+
+// function prod(num1,num2){
+//     return num1*num2;
+// }
+
+// function quo(num1,num2){
+//     return num1/num2;
+// }
+// console.log(add(2,2));
+// console.log(diff(2,2));
+// console.log(prod(2,2));
+// console.log(quo(2,2));
+
+
