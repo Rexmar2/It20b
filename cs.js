8d71f21630d759c0c20ec2043102b162f1a84f43
Rexmar2 <hazerulincuna@gmail.com>
2025-11-18 08:45:13 +0800

    Added funtion for conditional statement

---
+++ cs.js
@@ -0,0 +1,65 @@
+// function legalage(age){
+//     if(age>=18){
+//         console.log("You are an adult");
+//     } else {
+//         console.log("You are not adult");
+//     }
+// }
+
+// legalAge(20);
+
+// function computeGrade(grade){
+//   if(grade >= 94.5){
+//     console.log("1");
+//   } else if(grade >= 91.5){
+//     console.log("1.25");
+//     } else if(grade >= 89.5){
+//     console.log("1.50");
+//     } else if(grade >= 86.5){
+//     console.log("1.75");
+//     } else if(grade >= 84.5){
+//     console.log("2");
+//     }else{
+//         console.log("Not within the transmutation");
+//     }
+// }
+// computeGrade(90);
+
+// function login(username,password){
+
+//     if(username === "rexy"){
+       
+//         if (password ===1234)
+
+//          console.log("Welcome User");
+//     } else {
+    
+//         console.log("Invalid Credentials");
+
+//     }
+// }
+
+
+// login("rexy", 1234
+
+function ticketPrice(age){
+
+    if (age < 18){
+      if(age < 5 ){
+        console.log("Ticket: Free");
+      }else{
+        console.log("Ticket: Child Price");
+      }
+
+
+      }else{
+        if (age >=60){
+            console.log("Ticket: Senior Discount");
+        } else {
+            console.log("Ticket: Adult Price");
+        }
+    }
+}
+ticketPrice(70);
+
+
