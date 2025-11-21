6b7f823237213e4b6c3ba466ec922e0f9ad69193
Rexmar2 <hazerulincuna@gmail>
2025-11-21 08:16:32 +0800

    Added for-loop.js

---
+++ for-loop.js
@@ -0,0 +1,60 @@
+/*
+for(i=1 ;1<=5 ;i++){
+    console.log(i); }
+
+output = "";
+for(i=1 ;1<=5 ;i++){
+    output +=i + " ";
+}
+ console.log(output);
+
+
+output = "";
+for(i=5 ;i>=1 ;i--){
+    output +=i + " ";
+}
+ console.log(output);
+
+
+ reverse = "";
+ str = "Rex";
+
+ for(i=str.length; i>=0; i-- ){
+    reverse += str[i];
+
+ }
+ 
+ console.log(reverse)
+
+output = "";
+ arr = [4,2,3];
+
+ for(i-0; i<arr.length; i++){
+    output += arr[i] + " "
+ }
+    console.log(output);
+
+
+output = "";
+ arr = [4,2,3];
+
+ for(i<arr.length-1; i>=0;  i--){
+
+    output += arr[i] + " "
+ }
+     */
+
+ arr1 = [4,2,3];
+  arr1 = [6,7,4,2,0];
+   arr1 = [8,6,2,3];
+
+  function arrayTraverse(arr){
+    output= "";
+    for(i-0; i<arr.length; i++){
+        output += arr[i] + " "
+    }
+    arrayTraverse(arr1);
+      arrayTraverse(arr2);
+        arrayTraverse(arr3);
+
+  }
