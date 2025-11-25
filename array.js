6b6cad07d0fcd011334d7920e2f3973c874bf9a9
Rexmar2 <hazerulincuna@gmail.com>
2025-11-25 08:16:35 +0800

    Added delete method

---
+++ array.js
@@ -0,0 +1,60 @@
+class Arary{
+
+    constructor(){
+        this.data = {};
+        this.length = 0;
+
+        //Assign element to index
+        for(let i=0; i<initialData.length; i++){
+            this.data[i] = initialData[i];
+            this.length++;
+
+        }
+    }
+    traverse(){
+        let output = "";
+
+        for(let i=0; i<this.length; i++){
+            output +=this.data[i] + "";
+        }
+
+         console.log(output);
+        }
+
+        insert(index, element){
+            //Correcting Statement
+            if(index <0 || index > this.length){
+                console.log("Invalid Index");
+                return;
+            }
+            //Shifting Condition
+            for(let i=this.length; i>index; i--){
+                this.data[i] = this.data[i-1];
+            }
+            //After Condition
+          this.data[index] =element;
+          this.length++;
+    
+        }
+        delete(index){
+              if(index <0 || index > this.length){
+                console.log("Invalid Index");
+                return;
+            }
+             for(let i=this.length; i>index; i--){
+                this.data[i] = this.data[i-1];
+       
+           }
+              delete this.data[this.length-1];
+              this.length--;
+        }
+        
+    }
+    //
+    arr1 = new Array([4,2,3]);
+
+    arr1.traverse();
+    arr1.insert(0,9);
+    arr1.traverse();
+    arr1.delete(3);
+    arr1.traverse();
