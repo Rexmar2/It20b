b566c473e91ba4e4e519352bc73470c40c6f3f24
Rexmar2 <hazerulincuna@gmail>
2025-12-02 08:56:19 +0800

    Added insert_at_beginning

--- linkedlist.js
+++ linkedlist.js
@@ -10,13 +10,13 @@ class LinkedList{
     constructor(){
         this.head = null;
     }
-    insert_at_beginning(){
+    insert_at_beginning(element){
         const node = new Node(element);
         node.next  = this.head;
         this.head = node;
     }
     
-    treverse(){
+    traverse(){
         let current = this.head;
         let output = "";
 
@@ -28,10 +28,41 @@ class LinkedList{
     
 
     }
+    insert_at_end(element){
+        const node = new Node(element);
+
+        if(!this.head){
+            this.head = node;
+            return;
+        }
+        let current = this.head;
+        while(current.next){
+            current = current.next
+        }
+
+           current.next = node;
+    }
+    insert_after(target,element){
+        let current = this.head
+
+        while(current){
+            if(current.value === target){
+                const node = new Node(element);
+                node.next = current.next;
+                current.next = node;
+                return;
+            }
+             current = current.next;
+        }
+        console.log("Target not found");
+    }
+
 }
 
 let list1 = new LinkedList();
-list1.insert_at_beginning(4);
-list1.insert_at_beginning(3);
+list1.insert_at_end(4);
 list1.insert_at_beginning(2);
+list1.insert_at_end(3);
+console.log(list1.traverse());
+list1.insert_after(4,17);
 console.log(list1.traverse());
